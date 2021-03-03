65f1e25351fd8ef601eb6b7abd7af3f3bf90e40e
nikhi8850 <75842847+nikhi8850@users.noreply.github.com>
2021-03-03 19:25:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,68 @@
-var balloon,balloonAnimation;
-var ground,groundImage;
+var balloon, database;
+var position;
 
 function preload(){
-groundImage=loadAnimation("Image/Hot Air Ballon-01.png");
-balloonAnimation=loadAnimation("Image/Hot Air Ballon-02.png","Image/Hot Air Ballon-03.png","Image/Hot Air Ballon-04.png");
+  balloonImage1= loadAnimation("Image/Hot Air Ballon-01.png")
+  balloonImage2=loadAnimation("Image/Hot Air Ballon-02.png","Image/Hot Air Ballon-03.png","Image/Hot Air Ballon-04.png")
 }
 
-function setup() {
-  createCanvas(800,400);
-    
-  balloon=createSprite(400, 200, 50, 50);
-  balloon.addAnimation("ballon",balloonAnimation);
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(1000,1000);
 
-  ground.addAnimation("ground",groundImage);
+  ground=createSprite(100,100,500,500);
+  ground.addAnimation("ground",balloonImage1);
+  ground.scale=0.8
+
+  balloon = createSprite(250,250,10,10);
+  balloon.addAnimation("balloon",balloonImage2)
+  balloon.shapeColor = "red";
+  balloon.scale=0.5
+
+  
+
+  var balloonPosition = database.ref('balloon/position');
+  balloonPosition.on("value", readPosition, showError);
 }
 
-function draw() {
+function draw(){
+  background("white");
   
+    if(keyDown(LEFT_ARROW)){
+      balloon.addAnimation("balloon",balloonImage2)
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      balloon.addAnimation("balloon",balloonImage2)
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      balloon.addAnimation("balloon",balloonImage2)
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      balloon.addAnimation("balloon",balloonImage2)
+      writePosition(0,+1);
+    }
+    drawSprites();
   
-  drawSprites();
+}
+
+function writePosition(x,y){
+  database.ref('balloon/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
 }
